7131d735666c4f30afa52ad57261402e08c9adf6
mmmherma <marcosherma@gmail.com>
2020-04-09 10:50:52 +0200

    Added reindex matrix, multiplications and getElement

--- src/main/scala/com/github/optimizationwithmetaheuristics/combinatorialproblem/sa/QAP/QAP.scala
+++ src/main/scala/com/github/optimizationwithmetaheuristics/combinatorialproblem/sa/QAP/QAP.scala
@@ -1,5 +1,6 @@
 package com.github.optimizationwithmetaheuristics.combinatorialproblem.sa.QAP
 
+import com.github.optimizationwithmetaheuristics.continuousproblem.sa.HummelblauFunction.Matrix
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
@@ -21,29 +22,42 @@ object QAP extends App {
 
   // STEP 1. Init problem
   // Create distance matrix between departments
-  protected val distanceMatrix = Array(
-    Array(0, 1, 2, 3, 1, 2, 3, 4),
-    Array(1, 0, 1, 2, 2, 1, 2, 3),
-    Array(2, 1, 0, 1, 3, 2, 1, 2),
-    Array(3, 2, 1, 0, 4, 3, 2, 1),
-    Array(1, 2, 3, 4, 0, 1, 2, 3),
-    Array(2, 1, 2, 3, 1, 0, 1, 2),
-    Array(3, 2, 1, 2, 2, 1, 0, 1),
-    Array(4, 3, 2, 1, 3, 2, 1, 0)
-  )
+  protected var distanceMatrix = new Matrix(8)
+  distanceMatrix.setRow(Array(0, 1, 2, 3, 1, 2, 3, 4), 0)
+  distanceMatrix.setRow(Array(1, 0, 1, 2, 2, 1, 2, 3), 1)
+  distanceMatrix.setRow(Array(2, 1, 0, 1, 3, 2, 1, 2), 2)
+  distanceMatrix.setRow(Array(3, 2, 1, 0, 4, 3, 2, 1), 3)
+  distanceMatrix.setRow(Array(1, 2, 3, 4, 0, 1, 2, 3), 4)
+  distanceMatrix.setRow(Array(2, 1, 2, 3, 1, 0, 1, 2), 5)
+  distanceMatrix.setRow(Array(3, 2, 1, 2, 2, 1, 0, 1), 6)
+  distanceMatrix.setRow(Array(4, 3, 2, 1, 3, 2, 1, 0), 7)
+//  println("Distance matrix")
+//  distanceMatrix.printMatrix
   // Create flow matrix between departments
-  protected val flowMatrix = Array(
-    Array(0,5,2,4,1,0,0,6),
-    Array(5,0,3,0,2,2,2,0),
-    Array(2,3,0,0,0,0,0,5),
-    Array(4,0,0,0,5,2,2,10),
-    Array(1,2,0,5,0,10,0,0),
-    Array(0,2,0,2,10,0,5,1),
-    Array(0,2,0,2,0,5,0,10),
-    Array(6,0,5,10,0,1,10,0)
-  )
+  protected val flowMatrix = new Matrix(8)
+  flowMatrix.setRow(Array(0,5,2,4,1,0,0,6), 0)
+  flowMatrix.setRow(Array(5,0,3,0,2,2,2,0), 1)
+  flowMatrix.setRow(Array(2,3,0,0,0,0,0,5), 2)
+  flowMatrix.setRow(Array(4,0,0,0,5,2,2,10), 3)
+  flowMatrix.setRow(Array(1,2,0,5,0,10,0,0), 4)
+  flowMatrix.setRow(Array(0,2,0,2,10,0,5,1), 5)
+  flowMatrix.setRow(Array(0,2,0,2,0,5,0,10), 6)
+  flowMatrix.setRow(Array(6,0,5,10,0,1,10,0), 7)
+//  println(flowMatrix.printMatrix)
 
   // Initial solution
   protected var x0 = Array(1, 3, 0, 4, 2, 5, 6, 7)
+
+  // Reindex distance matrix
+  protected val reindexedDistanceMatrix = distanceMatrix
+  reindexedDistanceMatrix.reindex(x0)
+  /*// Reindex cost matrix
+  protected val reindexedFlowMatrix = flowMatrix
+  reindexedFlowMatrix.reindex(x0)
+  reindexedFlowMatrix.printMatrix*/
+
+  protected val test = reindexedDistanceMatrix
+  test.multiplication(flowMatrix)
+  test.printMatrix
 
 }
--- src/main/scala/com/github/optimizationwithmetaheuristics/continuousproblem/sa/HummelblauFunction/Matrix.scala
+++ src/main/scala/com/github/optimizationwithmetaheuristics/continuousproblem/sa/HummelblauFunction/Matrix.scala
@@ -2,8 +2,59 @@ package com.github.optimizationwithmetaheuristics.continuousproblem.sa.Hummelbla
 
 import org.slf4j.LoggerFactory
 
-class Matrix {
+import scala.collection.mutable.ArrayBuffer
+
+class Matrix(dimension: Int) {
 
   implicit val logger = LoggerFactory.getLogger(getClass.getName)
 
+  protected var matrix = Array.ofDim[Int](dimension, dimension)
+
+  def setRow(row: Array[Int], index: Int): Unit = {
+    for (i <- 0 to dimension-1) {
+      matrix(index)(i) = row(i)
+    }
+  }
+
+  def getElement(row: Int, col: Int): Int =
+    matrix(row)(col)
+
+  def reindex(newOrder: Array[Int]): Unit = {
+    var flipColsMatrix = ArrayBuffer.fill(dimension, dimension)(0)
+    var flipRowsMatrix = ArrayBuffer.fill(dimension, dimension)(0)
+
+    // Flip columns
+    for (j <- 0 to (dimension-1)) {   // Row for
+      for (k <- 0 to (dimension-1)) { // Column for
+        flipColsMatrix(k)(j) = matrix(k)(newOrder(j))
+      }
+    }
+
+    // Flip rows
+    for (j <- 0 to (dimension-1)) {   // Row for
+      for (k <- 0 to (dimension-1)) { // Column for
+        flipRowsMatrix(j)(k) = flipColsMatrix(newOrder(j))(k)
+      }
+    }
+
+    for (i <- 0 to (dimension-1)) {
+      setRow(flipRowsMatrix(i).toArray, i)
+    }
+  }
+
+  def multiplication(matrix2: Matrix): Unit = {
+    for (j <- 0 to (dimension-1)) {   // Row for
+      for (k <- 0 to (dimension-1)) { // Column for
+        matrix(j)(k) = matrix(j)(k) * matrix2.getElement(j, k)
+      }
+    }
+  }
+
+  def printMatrix: Unit =
+    for (i <- 0 to dimension-1) {
+      for (j <- 0 to dimension-1) {
+        print(matrix(i)(j) + " ")
+      }
+      println()
+    }
 }
